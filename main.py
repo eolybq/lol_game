aeb506bfa02ae24f01353350575200f010412579
eolybq <164062935+eolybq@users.noreply.github.com>
2024-04-01 22:00:57 +0200

    vojta je gej 2

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ class Player:
 		self.rect = self.image.get_rect(topleft=(x,y))
 		
 sion = Player((width - 50) // 2,height -150, 'sion.png')
+
+
+
+
+class Bullets:
+	def __init__(self, image_path):
+		self.image = pygame.image.load(image_path).convert_alpha()
+		self.rect = self.image.get_rect(topleft=(x, y))
+		
+	def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
